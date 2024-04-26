74d4cbeca062991953ec18733440c220922b3acd
Yash Rikhi <yashrikhi@gmail.com>
2024-04-26 17:15:21 +0530

    Weather Widget Data added

--- src/WeatherWidget.jsx
+++ src/WeatherWidget.jsx
@@ -1,25 +1,26 @@
 import React from "react";
 import './WeatherWidget.css';
 
-const WeatherWidget = ({cityName}) => {
+const WeatherWidget = ({cityName, weatherData}) => {
+
     return (
         <div class="col-md-4">
         <div class="card">
-            <span class="icon"><img class="img-fluid" src="https://img.icons8.com/emoji/96/000000/sun-emoji.png"/></span>
+            <span class="icon me-1"><img class="img-fluid" src={weatherData && weatherData.current && weatherData.current.condition && weatherData.current.condition.icon}/></span>
             <div class="title"><p>{cityName}</p></div>
-            <div class="temp">+20<sup>&deg;</sup></div>
+            <div class="temp">{weatherData && weatherData.current && weatherData.current.temp_c}<sup>&deg;</sup></div>
                 <div class="row">
                     <div class="col-4">
                         <div class="header">General</div>
-                        <div class="value">Sunny</div>
+                        <div class="value">{weatherData && weatherData.current && weatherData.current.condition && weatherData.current.condition.text}</div>
                     </div>
                     <div class="col-4">
-                        <div class="header">Air pollution</div>
-                        <div class="value">47</div>
+                        <div class="header">Humidity</div>
+                        <div class="value">{weatherData && weatherData.current && weatherData.current.humidity}</div>
                     </div>
                     <div class="col-4">
-                        <div class="header">Fire</div>
-                        <div class="value">No</div>
+                        <div class="header">Feels Like</div>
+                        <div class="value">{weatherData && weatherData.current && weatherData.current.feelslike_c}</div>
                     </div>
                 </div>
                 
